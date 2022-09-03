0c9f95788c8b6f1816b5a48535255cb6babb4944
Alex173 <sashakechedzhy@gmail.com>
2022-09-03 20:25:00 +0300

    Start configuring main screen

--- Expify/Flow/Main/MainViewController.swift
+++ Expify/Flow/Main/MainViewController.swift
@@ -6,8 +6,32 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
+    
+    private let settingsButton: UIButton = {
+        let button = UIButton()
+        let icon = UIImage(systemName: "gearshape.fill")?.withRenderingMode(.alwaysTemplate)
+        button.setImage(icon, for: .normal)
+        button.tintColor = R.color.accent()
+        button.imageView?.contentMode = .scaleAspectFit
+        button.snp.makeConstraints {
+            $0.width.height.equalTo(32)
+        }
+        button.contentVerticalAlignment = .fill
+        button.contentHorizontalAlignment = .fill
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = R.color.accent()
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.text = "Main Screen"
+        return label
+    }()
 
     private var viewModel: MainViewModelInteface
     
@@ -22,7 +46,29 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureView()
+    }
+    
+    private func configureView() {
+        view.backgroundColor = UIColor.systemBackground
+        configureSettingsButton()
+        configureTitleLabel()
+    }
+    
+    private func configureSettingsButton() {
+        view.addSubview(settingsButton)
+        settingsButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(16)
+            $0.trailing.equalToSuperview().inset(16)
+        }
+    }
+    
+    private func configureTitleLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(settingsButton.snp.top)
+        }
     }
 
 
