c274e6e1dfe7413acb43eae76c021814ceeb7282
Alex173 <sashakechedzhy@gmail.com>
2022-09-03 18:22:05 +0300

    Add Expense CoreData entity

--- Expify/Managers/CoreDataManager/Expense+CoreDataClass.swift
+++ Expify/Managers/CoreDataManager/Expense+CoreDataClass.swift
@@ -1,5 +1,5 @@
 //
-//  Category+CoreDataClass.swift
+//  Expense+CoreDataClass.swift
 //  
 //
 //  Created by Alex173 on 03.09.2022.
@@ -9,11 +9,11 @@
 import Foundation
 import CoreData
 
-@objc(Category)
-public class Category: NSManagedObject {
+@objc(Expense)
+public class Expense: NSManagedObject {
     convenience init() {
         
-        let entity = NSEntityDescription.entity(forEntityName: "Category", in: CoreDataManager.instance.managedObjectContext)
+        let entity = NSEntityDescription.entity(forEntityName: "Expense", in: CoreDataManager.instance.managedObjectContext)
         
         self.init(entity: entity!, insertInto: CoreDataManager.instance.managedObjectContext)
     }
