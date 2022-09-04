64f92d59edc653c760b31b11ececf704ee689d04
Alex173 <sashakechedzhy@gmail.com>
2022-09-04 19:50:49 +0300

    Update CoreDataClasses

--- Expify/Managers/CoreDataManager/Category+CoreDataProperties.swift
+++ Expify/Managers/CoreDataManager/Category+CoreDataProperties.swift
@@ -19,5 +19,26 @@ extension Category {
     @NSManaged public var dateCreated: Date?
     @NSManaged public var id: UUID?
     @NSManaged public var name: String?
+    @NSManaged public var expenses: NSSet?
+    
+}
+
+extension Category {
+
+    @objc(addExpenseObject:)
+    @NSManaged public func addExpenseObject(_ value: Expense)
+
+    @objc(removeExpenseObject:)
+    @NSManaged public func removeExpenseObject(_ value: Expense)
+
+    @objc(addExpenses:)
+    @NSManaged public func addExpenses(_ values: NSSet)
+
+    @objc(removeExpenses:)
+    @NSManaged public func removeExpenses(_ values: NSSet)
+
+}
+
+extension Category : Identifiable {
 
 }
